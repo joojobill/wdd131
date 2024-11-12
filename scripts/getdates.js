75196b9e2f60c9c9a6813f868a030ee25e8c3e05
joojobill <hoo21001@byui.edu>
2024-11-12 18:24:06 +0000

    mmhome

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,11 +1,4 @@
-// Get the current year
 const currentYear = new Date().getFullYear();
-
-// Set the current year in the first <span> element
 document.getElementById('currentyear').textContent = `@ ${currentYear} Isaac Hooper Ghana`;
-
-// Get the last modified date from the document
 const lastModified = document.lastModified;
-
-// Set the last modified date in the second <span> element
 document.getElementById('lastModified').textContent = `Last modified: ${lastModified}`;
