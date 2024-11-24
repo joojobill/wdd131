812c3b7af882eb766f172e636903a2c8951d5bb6
joojobill <hoo21001@byui.edu>
2024-11-24 14:13:58 +0000

    place1

---
+++ scripts/place.js
@@ -0,0 +1,4 @@
+const currentYear = new Date().getFullYear();
+document.getElementById('currentyear').textContent = `@ ${currentYear} Isaac Hooper Ghana`;
+const lastModified = document.lastModified;
+document.getElementById('lastModified').textContent = `Last modified: ${lastModified}`;
